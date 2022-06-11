45d92c0ed668ea4bf427d4c8b5e925f174d444f8
DibeRebecca <rebe.taboukouna@gmail.com>
2022-06-11 17:19:41 +0000

    amazing clone

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    './public/index.html',
+    './public/style.css',
+    './src/style.css',
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "bookmark-purple":"#5267DF",
+        "bookmark-red":"#FA5959",
+        "bookmark-blue":"#1e40af",
+        "bookmark-grey":"#9194A2",
+        "bookmark-white":"#f7f7f7",
+        "bookmark-green":"#bbf7d0",
+        "bookmark-yellow":"#fb923c",
+        "bookmark-cendre":"#e2e8f0",
+        "bookmark-violet":"#5b21b6",
+        
+        
+      }
+    },
+    fontFamily:{
+      Poppins:["Poppins, sans-serif"],
+    },
+    container:{
+      center:true,
+      padding:"1rem",
+      screens:{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px",
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
